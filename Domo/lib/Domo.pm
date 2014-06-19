d8a1667ad29e325cfc8c9470396d28edf9acafe5
epierre <epierre@e-nef.com>
2014-06-19 23:28:49 +0200

    Corrected switches + waiting for Imperihome next version for corrections #826 Added Temp, Hygro, mBar, UV, Wind, Rain Added Scenes

--- Domo/lib/Domo.pm
+++ Domo/lib/Domo.pm
@@ -20,11 +20,17 @@ get '/' => sub {
 
 get '/rooms' => sub {
        #Room list
-  return {"rooms" => [ { "id"=> "noroom", "name"=> "noroom" }]};
+  return {"rooms" => [ 
+		{ "id"=> "noroom", "name"=> "noroom" },
+		{ "id"=> "Switches", "name"=> "Switches" },
+		{ "id"=> "Scenes", "name"=> "Scenes" },
+		{ "id"=> "Temp", "name"=> "Weather" },
+		{ "id"=> "Utility", "name"=> "Utility" },
+			]};
 };
 
 get '/system' => sub {
- return {"id"=> "01:02:03:04:05:06","apiversion"=> 1};
+ return {"id"=> "MyDomoAtHome","apiversion"=> 1};
 };
 
 
@@ -86,36 +92,128 @@ if ($actionName eq 'setStatus') {
 
 get '/devices' => sub {
 	my $feed={ "devices" => []};
-	my $trendsurl = "http://192.168.0.24:8080/json.htm?type=devices&filter=all&used=true&order=Name";
+	my $system_url = "http://192.168.0.24:8080/json.htm?type=devices&filter=all&used=true&order=Name";
 	my $ua = LWP::UserAgent->new();
-	$ua->agent("Some/string");
-	my $json = $ua->get( $trendsurl );
-	warn "Could not get $trendsurl!" unless defined $json;
+	$ua->agent("MyDomoREST/$VERSION");
+	my $json = $ua->get( $system_url );
+	warn "Could not get $system_url!" unless defined $json;
 	# Decode the entire JSON
 	my $decoded = JSON->new->utf8(0)->decode( $json->decoded_content );
 	my @results = @{ $decoded->{'result'} };
 	foreach my $f ( @results ) {
-		 if ($f->{"SwitchType"}) {
-			#print $f->{"idx"} . " " . $f->{"Name"} . " " . $f->{"Status"} . $f->{"LastUpdate"}."\n";
 			my $dt = Time::Piece->strptime($f->{"LastUpdate"},"%Y-%m-%d %H:%M:%S");
 			my $name=$f->{"Name"};
 			$name=~s/\s/_/;
 			$name=~s/\s/_/;
 			$name=~s/\//_/;
 			$name=~s/%/P/;
+		 if ($f->{"SwitchType"}) {
+			#print $f->{"idx"} . " " . $f->{"Name"} . " " . $f->{"Status"} . $f->{"LastUpdate"}."\n";
 			$name.="_E";
 			my $bl=$f->{"Status"};my $rbl;
 			if ($bl eq "On") { $rbl=1;}
 			elsif ($bl eq "Off") { $rbl=0;}
 			else { $rbl=$bl;}
-			#print "L0 $name/$rbl/".$dt->datetime."\n";
 
-			my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevSwitch", "room" => "noroom", params =>[]};
-			push (@{$feeds->{'params'}}, {"key" => "Status", "value" =>$bl} );
+			my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevSwitch", "room" => "Switches", params =>[]};
+			push (@{$feeds->{'params'}}, {"key" => "Status", "value" =>"$rbl"} );
 			push (@{$feed->{'devices'}}, $feeds );
-		};
+		} else {
+			if ($f->{"Type"} eq "Energy") {
+				#DevElectricity Electricity consumption sensor
+				#Watts  Current consumption     Watt
+				#ConsoTotal     Current total consumption       kWh
+				#"Type" : "Energy", "SubType" : "CM180", "Usage" : "408 Watt", "Data" : "187.054 kWh"
+				my ($usage)= ($f->{"Usage"} =~ /(\d+) Watt/);
+				my ($total)= ($f->{"Data"} =~ /(\d+) kWh/);
+				my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevElectricity", "room" => "Utility", params =>[]};
+				push (@{$feeds->{'params'}}, {"key" => "Watts", "value" =>$usage, "unit" => "W"} );
+				push (@{$feed->{'devices'}}, $feeds );
+			}  elsif (($f->{"Type"} =~ "Temp")||($f->{"Type"} =~ "Humidity"))  {
+				my @type=split(/ \+ /,$f->{"Type"});
+				my $cnt;
+				foreach my $curs (@type) {
+					$cnt++;
+					if ($curs eq "Temp") {
+						#DevTemperature Temperature sensor
+						#Value  Current temperature     °C
+						#"Temp" : 21.50,  "Type" : "Temp + Humidity" / Type" : "Temp",
 
-	};
+						my $feeds={params =>[],"room" => "Temp","type" => "DevTemperature","name" => $name, "id" => $f->{"idx"}."_".$cnt};
+						my $v=$f->{"Temp"};
+						push (@{$feeds->{'params'}}, {"key" => "Value", "value" => "$v", "unit" => "°C"} );
+						push (@{$feed->{'devices'}}, $feeds );
+					} elsif ($curs eq "Humidity") {
+						#DevHygrometry  Hygro sensor
+						#Value  Current hygro value     %
+						# "Humidity" : 52  "Type" : "Temp + Humidity" / Type" : "Humidity",
+
+						my $feeds={"id" => $f->{"idx"}."_".$cnt, "name" => $name, "type" => "DevHygrometry", "room" => "Temp", params =>[]};
+						my $v=$f->{"Humidity"};
+						push (@{$feeds->{'params'}}, {"key" => "Value", "value" => "$v", "unit" => "%"} );
+						push (@{$feed->{'devices'}}, $feeds );
+					} elsif ($curs eq "Baro") {
+						#DevPressure    Pressure sensor
+						#Value  Current pressure        mbar
+						#"Barometer" : 1022, "Type" : "Temp + Humidity + Baro"
+						my $feeds={"id" => $f->{"idx"}."_".$cnt, "name" => $name, "type" => "DevPressure", "room" => "Temp", params =>[]};
+						my $v=$f->{"Barometer"};
+						push (@{$feeds->{'params'}}, {"key" => "Value", "value" => "$v", "unit" => "mbar"} );
+						push (@{$feed->{'devices'}}, $feeds );
+					}
+				}
+			}  elsif ($f->{"Type"} eq "Rain")  {
+				#DevRain        Rain sensor
+				#Value  Current instant rain value      mm/h
+				#Accumulation   Total rain accumulation mm
+				#"Rain" : "0.0", "RainRate" : "0.0", "Type" : "Rain"
+						my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevRain", "room" => "Temp", params =>[]};
+						my $v0=$f->{"RainRate"};
+						my $v1=$f->{"Rain"};
+						push (@{$feeds->{'params'}}, {"key" => "Accumulation", "value" => "$v1", "unit" => "mm"} );
+						push (@{$feeds->{'params'}}, {"key" => "Value", "value" => "$v0", "unit" => "mm/h"} );
+						push (@{$feed->{'devices'}}, $feeds );
+			} elsif ($f->{"Type"} eq "UV")  {
+				#DevUV  UV sensor
+				#Value  Current UV index        index
+				# "Type" : "UV","UVI" : "6.0"
+				my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevUV", "room" => "Temp", params =>[]};
+				my $v=$f->{"UVI"};
+				push (@{$feeds->{'params'}}, {"key" => "Value", "value" => "$v"} );
+				push (@{$feed->{'devices'}}, $feeds );
+			}
+
+		}
+
+
+	}; 
+	#Get Scenes
+	$system_url="http://192.168.0.24:8080/json.htm?type=scenes";
+	$json = $ua->get( $system_url );
+	warn "Could not get $system_url!" unless defined $json;
+	# Decode the entire JSON
+	$decoded = JSON->new->utf8(0)->decode( $json->decoded_content );
+	@results = @{ $decoded->{'result'} };
+	foreach my $f ( @results ) {
+			my $dt = Time::Piece->strptime($f->{"LastUpdate"},"%Y-%m-%d %H:%M:%S");
+debug($dt->strftime("%Y-%m-%d %H:%M:%S"));
+			my $name=$f->{"Name"};
+			$name=~s/\s/_/;
+			$name=~s/\s/_/;
+			$name=~s/\//_/;
+			$name=~s/%/P/;
+			#DevScene       Scene (launchable)
+			#LastRun        Date of last run        N/A
+			#"idx" : "3", "Name" : "Alerte", "Type" : "Scene", "LastUpdate" : "2014-03-18 22:17:18"
+			my $feeds={"id" => $f->{"idx"}, "name" => $name, "type" => "DevScene", "room" => "Scene", params =>[]};
+			my $v=$dt->strftime("%Y-%m-%d %H:%M:%S");
+			push (@{$feeds->{'params'}}, {"key" => "LastRun", "value" => "$v"} );
+			push (@{$feed->{'devices'}}, $feeds );
+	}
+	#DevGenericSensor      Generic sensor (any value)
+	#Value  Current value   N/A
+
+
 
 	return($feed);
 	return { success => true};
