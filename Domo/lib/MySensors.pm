74d58e34f726e920b771d1565555ded6601e1424
empierre <epierre@e-nef.com>
2014-06-26 15:47:36 +0200

    Update MySensors.pm
    necessary evil

--- Domo/lib/MySensors.pm
+++ Domo/lib/MySensors.pm
@@ -1,4 +1,8 @@
 package Domo;
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# version 2 as published by the Free Software Foundation.
+# Author: epierre
 use Dancer ':syntax';
 use DBI;
 use File::Spec;
