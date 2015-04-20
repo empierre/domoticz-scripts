ffb299b2e7a57edffcc7f45844c619bd5759d471
empierre <epierre@e-nef.com>
2015-04-20 10:37:45 +0200

    Update AIQ-LCSQA.pl
    added a bit of docs

--- AIQ-LCSQA.pl
+++ AIQ-LCSQA.pl
@@ -7,7 +7,19 @@ use warnings;                   # Good practice
 use utf8;    
 use POSIX;
 use feature     qw< unicode_strings >;
+#Support: domoticz@e-nef.com ou forum Domoticz
 
+#A adapter à votre configuration:
+my $domo_ip="192.168.0.28";
+my $domo_port="8080";
+my $agglomeration="PARIS";
+my $dz_ind=211;
+my $dz_o3=220;
+my $dz_no2=221;
+my $dz_so2=222;
+my $dz_pm10=219;
+
+#Ne pas toucher en dessous
 my $trendsurl = " http://www.lcsqa.org/surveillance/indices/prevus/jour/xml/";
 
 my $json = get( $trendsurl );
@@ -21,17 +33,17 @@ my $decoded = $xml->XMLin( $json );
 #print Dumper $decoded;
 
 foreach my $f ( @{$decoded->{node}} ) {
-  if ($f->{"agglomeration"}eq "PARIS") {
+  if ($f->{"agglomeration"}eq $agglomeration) {
 	print $f->{"valeurIndice"} . " " . $f->{"SousIndiceO3"} . " " . $f->{"SousIndiceNO2"} ." ". $f->{"SousIndiceSO2"} . " " .$f->{"SousIndicePM10"} . "\n";
 	my $payload=$f->{"valeurIndice"};
-	if (isdigit($payload)) { `curl -s "http://192.168.0.28:8080/json.htm?type=command&param=udevice&idx=211&nvalue=$payload"`; }
+	if (isdigit($payload)) { `curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=$dz_ind&nvalue=$payload"`; }
 	$payload=$f->{"SousIndiceO3"};
-	if (isdigit($payload)) { `curl -s "http://192.168.0.28:8080/json.htm?type=command&param=udevice&idx=220&nvalue=$payload"`; }
+	if (isdigit($payload)) { `curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=$dz_o3&nvalue=$payload"`; }
 	$payload=$f->{"SousIndiceNO2"};
-	if (isdigit($payload)) { `curl -s "http://192.168.0.28:8080/json.htm?type=command&param=udevice&idx=221&nvalue=$payload"`; }
+	if (isdigit($payload)) { `curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=$dz_no2&nvalue=$payload"`; }
 	$payload=$f->{"SousIndiceSO2"};
-	if (isdigit($payload)) { `curl -s "http://192.168.0.28:8080/json.htm?type=command&param=udevice&idx=222&nvalue=$payload"`; }
+	if (isdigit($payload)) { `curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=$dz_so2&nvalue=$payload"`; }
 	$payload=$f->{"SousIndicePM10"};
-	if (isdigit($payload)) { `curl -s "http://192.168.0.28:8080/json.htm?type=command&param=udevice&idx=219&nvalue=$payload"`; }
+	if (isdigit($payload)) { `curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=$dz_pm10&nvalue=$payload"`; }
   }
 }
