6efe5ca6b12379129189bbbd203db6d2a4b13c15
Emmanuel PIERRE <you@example.com>
2016-06-21 21:24:32 +0200

    upgraded for pid file

--- mysensors-gw.pl
+++ mysensors-gw.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl -w
+#  License: Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
 use warnings;
 use strict;
-use POSIX qw(strftime);
+use POSIX qw(strftime ceil floor);
 use Device::SerialPort;
 use IO::Handle;
 use DateTime;
@@ -10,7 +11,8 @@ use DBI;
 use Config::Simple;
 
 # Initialization strings
-my $conf = ".conf-mysensors";
+my $base="/home/pi/";
+my $conf="/home/pi/.conf-mysensors";
 my $ccnt;
 my $cfg;
 my ($count, $string, $radioId, $value);
@@ -21,9 +23,14 @@ my $domo_port = $Config->{"Internal.domo_port"};
 my $hardware_id = $Config->{"Internal.hardware_domo"};
 my $dbh;
 my %sensor_tab;
+my $TSurl;
+
+open(FIC,'>/tmp/mysensors-gw.pid')||warn $!;
+print FIC $$;
+close(FIC);
 
 # SQLite database connexion
-if (! -e "mysensors.db") {
+if (! -e "/home/pi/mysensors.db") {
 	$dbh=&init_database();
 	&create_table($dbh);
 } else {$dbh=&init_database();}
@@ -50,7 +57,7 @@ sleep $sleep;
 
 # Now parse output
 my @vals;
-open(FIC,">>log-gw.txt")||die $!;
+open(FIC,">>".$base."log-gw.txt")||die $!;
 print FIC "Starting\n";
 FIC->autoflush(1);
 while(1) {
@@ -59,13 +66,13 @@ while(1) {
 
        ($count, $string) = $ob->read(255);
 
-       #print "$ccnt $string\n\n";
+       #print "# $ccnt $string\n\n";
        @vals  = split("\n", $string);
        foreach (@vals) {
                $_=~ s/\t/\=/;
                $_=~ s/\r//;
                $_=~ s/\n//;
-                my ($radioId, $childId,$messageType,$subType,$payload) = split(";", $_);
+                my ($radioId,$childId,$messageType,$ack,$subType,$payload) = split(";", $_);
                 if (! $childId) {$childId="0";}
                 if (! $messageType) {$messageType="0";}
                 if (! $subType) {$subType="0";}
@@ -76,13 +83,13 @@ while(1) {
                 #$value = $value/1000 if $radioId =~ /I/g;
                 my $dt = DateTime->now(time_zone=>'local');
                 my $date=join ' ', $dt->ymd, $dt->hms;
-                print "$date $radioId $childId $messageType $subType $payload\n";
+                print "$date $radioId $childId $messageType $ack $subType $payload\n";
                 if ($radioId>=0) {
-                        print FIC "$date $radioId $childId $messageType $subType $payload\n";
+                        print FIC "$date $radioId $childId $messageType $ack $subType $payload\n";
                 }
-                if (($messageType==4)&&($subType==5)) {
+                if (($messageType==3)&&($subType==3)) {
 			#Answer the node ID
-                        my $msg = "$radioId;$childId;4;5;8\n";
+                        my $msg = "$radioId;$childId;3;0;4;20\n";
                         my $co = $ob->write($msg);
                         warn "write failed\n" unless ($co);
                         print "$date W ($co) : $msg \n";
@@ -92,12 +99,8 @@ while(1) {
                 if (($messageType==2)&&($subType==24)) {
 			#Answer the node VAR_1
 			my $msg;
-			if ($radioId==2) {
-				my $val=$sensor_tab{$radioId}->{$subType}||36890;
-	                        $msg = "$radioId;$childId;3;24;$val\n";
-			} else {
-	                        $msg = "$radioId;$childId;3;24;10\n";
-			}
+			my $val=$sensor_tab{$radioId}->{$subType}||36890;
+	                $msg = "$radioId;$childId;0;3;24;$val\n";
                         my $co = $ob->write($msg);
                         warn "write failed\n" unless ($co);
                         print "$date W ($co) : $msg \n";
@@ -106,7 +109,7 @@ while(1) {
 		}
                 if (($messageType==4)&&($subType==13)) {
 			#Answer we are Metric
-                        my $msg = "$radioId;$childId;4;13;M\n";
+                        my $msg = "$radioId;$childId;0;4;13;M\n";
                         my $co = $ob->write($msg);
                         warn "write failed\n" unless ($co);
                         print "$date W ($co) : $msg \n";
@@ -132,26 +135,149 @@ while(1) {
 			print "sending to DZ 164 $payload\n";
 			`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=164&svalue=$temp;$payload;2" &`;
 		}
- 		if (($messageType==1)&&($subType==24)) {
-			# save a VAR
+                if (($messageType==1)&&($subType==16)) {
+			# Read the leaf wetness 
 			$sensor_tab{$radioId}->{$subType}=$payload;
 			&update_or_insert($radioId,$subType,$payload);
-			if ($radioId==3) {
+			my $val=100-($payload);my $h=1;
+			if ($val>50) {$h=2} elsif ($val<25) {$h=0} else {$h=1};
+			print "sending to DZ 243 $val\n";
+			`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=243&nvalue=$val&svalue=0;$h" &`;
+		}
+ 		if (($messageType==1)&&($subType==24)) {
+			# AqCO2
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			&update_or_insert($radioId,$subType,$payload);
+			if ($radioId==7) {
 				print "sending to DZ 124 $payload\n";
 				`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=124&nvalue=$payload" &`;
 			}
 		}
- 		if (($messageType==1)&&($subType==4)) {
-			# save a BARO
+ 		if (($messageType==1)&&($subType==37)) {
+			# save a DUST_LEVEL
 			$sensor_tab{$radioId}->{$subType}=$payload;
 			&update_or_insert($radioId,$subType,$payload);
-			if ($radioId==4) {
+			if ($radioId==7) {
+				# AqCO2
+				print "sending to DZ 124 $payload\n";
+				`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=124&nvalue=$payload" &`;
+			}
+			if ($radioId==3) {
+				if ($childId==0) {#PM10
+					print "sending to DZ 244 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=244&nvalue=$payload" &`;
+
+				} elsif ($childId=1) {#PM25
+					print "sending to DZ 245 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=245&nvalue=$payload" &`;
+
+				}
+			}
+			if ($radioId==11) {
+				if ($childId==0) {#PM10
+					print "sending to DZ 208 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=208&nvalue=$payload" &`;
+
+				} elsif ($childId=1) {#PM25
+					print "sending to DZ 225 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=225&nvalue=$payload" &`;
+
+				}
+			}
+		}
+ 		if (($messageType==1)&&($subType>=40)) {
+			# save a VAR
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			&update_or_insert($radioId,$subType,$payload);
+			if (($radioId==4)&&($subType==43)) {
+					print "sending $payload to DZ 425 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=425&nvalue=$payload" &`;
+			}
+			if (($radioId==8)||($radioId==18)) {
+				if ($subType==40) {#Smoke
+					print "sending $payload to DZ 223 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=223&nvalue=$payload" &`;
+				} elsif ($subType==41) {#LPG
+					print "sending $payload to DZ 226 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=226&nvalue=$payload" &`;
+				} elsif ($subType==42) {#O3
+					print "sending $payload to DZ 210 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=210&nvalue=$payload" &`;
+				} elsif ($subType==43) {
+					if ($childId==0) { 
+						$payload*=1000;
+						print "sending $payload to DZ 427 $payload\n";
+						`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=427&nvalue=$payload" &`;
+					} elsif ($childId==1) {
+						$payload*=1000;
+						print "sending $payload to DZ 209 $payload\n";
+						`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=209&nvalue=$payload" &`;
+					} elsif ($childId==2) {
+						$payload*=1000;
+						print "sending $payload to DZ 428 $payload\n";
+						`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=428&nvalue=$payload" &`;
+					}
+				} elsif ($subType==44) {#CO
+					print "sending $payload to DZ 209 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=209&nvalue=$payload" &`;
+				} elsif ($subType==45) {#PM10
+					#print "sending $payload to DZ 208 $payload\n";
+					#`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=208&nvalue=$payload" &`;
+				} elsif ($subType==46) {#SO2
+					print "sending $payload to DZ 224 $payload\n";
+					`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=224&nvalue=$payload" &`;
+				}
+			}
+		}
+ 		if (($messageType==1)&&($subType==4)) {
+			# save a BARO
+			if ($payload>700) {
+				$sensor_tab{$radioId}->{$subType}=$payload;
+				&update_or_insert($radioId,$subType,$payload);
 				print "sending to DZ 198 $payload\n";
 				`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=198&svalue=$payload" &`;
 			}
 		}
+ 		if (($messageType==1)&&($subType==11)) {
+			# save a UV
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			&update_or_insert($radioId,$subType,$payload);
+			print "sending to DZ 423 $payload\n";
+			`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=423&svalue=$payload" &`;
+		}
+ 		if (($messageType==1)&&($subType==23)) {
+			# save a Lux
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			&update_or_insert($radioId,$subType,$payload);
+			print "sending to DZ 361 $payload\n";
+			`curl -s "http://$domo_ip:$domo_port/json.htm?type=command&param=udevice&idx=361&svalue=$payload" &`;
+		}
+ 		if (($messageType==1)&&($subType==38)) {
+			# save a Voltage
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			if ($childId==10) {
+				$TSurl="http://api.thingspeak.com/update?key=BCZ675JM3V43QYBA";
+				$TSurl.="&field1=$payload";	
+			} elsif ($childId==11) {
+				$TSurl.="&field3=$payload";	
+			}
+			#print "sending to TS $payload $TSurl\n";
+			#`curl -s $TSurl &`;
+		}
+ 		if (($messageType==1)&&($subType==39)) {
+			# save a Current
+			$sensor_tab{$radioId}->{$subType}=$payload;
+			#my $TSurl="http://api.thingspeak.com/update?key=BCZ675JM3V43QYBA";
+			if ($childId==10) {
+				$TSurl.="&field2=$payload";	
+			} elsif ($childId==11) {
+				$TSurl.="&field4=$payload";	
+				print "sending to TS $payload $TSurl\n";
+				`curl -s "$TSurl" &`;
+			}
+		}
  		if (($messageType==1)&&($subType==35)) {
-			# Read the Temp value
+			# Watermeter
 			$sensor_tab{$radioId}->{$subType}=$payload;
 			&update_or_insert($radioId,$subType,$payload);
 			my $hum=$sensor_tab{$radioId}->{1}||0;
@@ -252,15 +378,15 @@ sub create_table {
 	}
 }
 sub insert_sensor {
-	my $stmt = qq(INSERT INTO sensors (id,subtype,value)
-	      VALUES ($_[0], $_[1], $_[2] ));
-	my $rv = $dbh->do($stmt) or die $DBI::errstr;
-	$sth->finish();
+	#my $stmt = qq(INSERT INTO sensors (id,subtype,value)
+	#      VALUES ($_[0], $_[1], $_[2] ));
+	#my $rv = $dbh->do($stmt) or die $DBI::errstr;
+	#$sth->finish();
 }
 sub update_sensor {
-	my $stmt = qq(UPDATE sensors set value=$_[2] where id=$_[0] and subtype=$_[1] );
-	my $rv = $dbh->do($stmt) or die $DBI::errstr;
-	$sth->finish();
+	#my $stmt = qq(UPDATE sensors set value=$_[2] where id=$_[0] and subtype=$_[1] );
+	#my $rv = $dbh->do($stmt) or die $DBI::errstr;
+#	$sth->finish();
 }
 sub update_or_insert {
 	my $sth = $dbh->prepare( "SELECT value FROM sensors WHERE id=$_[0] AND subtype=$_[1]");
